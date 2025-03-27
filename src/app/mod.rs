94da31e94c82df128b324a9ca218d4705a1c27fe
Kalsifer-742 <kalsifer742@gmail.com>
2025-03-27 18:31:43 +0100

    first complete program: opening a black window

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,20 +1,19 @@
-use std::{clone, sync::Arc};
+use std::sync::Arc;
 
 use render_context::RenderContext;
 use vulkano::{
-    command_buffer::allocator::StandardCommandBufferAllocator,
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
+        RenderPassBeginInfo, SubpassBeginInfo, SubpassContents,
+    },
     device::{
-        self,
         physical::{PhysicalDevice, PhysicalDeviceType},
         Device, DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo, QueueFlags,
     },
-    image::{view::ImageView, Image, ImageUsage},
     instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
-    pipeline::graphics::viewport::Viewport,
-    render_pass::{self, Framebuffer, FramebufferCreateInfo, RenderPass},
-    single_pass_renderpass,
-    swapchain::{self, Surface, Swapchain, SwapchainCreateInfo},
-    sync, VulkanLibrary,
+    swapchain::{self, Surface, SwapchainPresentInfo},
+    sync::{self, GpuFuture},
+    Validated, VulkanError, VulkanLibrary,
 };
 use winit::{
     application::ApplicationHandler,
@@ -26,15 +25,96 @@ use winit::{
 mod render_context;
 
 pub struct App {
+    instance: Arc<Instance>,
+    device: Arc<Device>,
+    command_buffer_allocator: Arc<StandardCommandBufferAllocator>,
+    queue: Arc<Queue>,
     render_context: Option<RenderContext>,
 }
 
 impl App {
     //what is () as a generic T which is static?
     pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let library = VulkanLibrary::new().unwrap();
+        let extensions = Surface::required_extensions(event_loop).unwrap();
+
+        let instance = Instance::new(
+            library,
+            // structs are passed as argument to resemble how the Vulkan API works
+            InstanceCreateInfo {
+                enabled_extensions: extensions,
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY, //this is need to run on macOS trough MoltenVK
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::empty()
+        };
+
+        let (physical_device, queue_family_index) =
+            Self::select_physical_device(&instance, &device_extensions, &event_loop);
+
+        //logical/software device, queues associated to the device
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: device_extensions,
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
+            device.clone(),
+            Default::default(),
+        ));
+        let queue = queues.next().unwrap(); //selecting a random queue
+
         Self {
+            instance,
+            device,
+            command_buffer_allocator,
+            queue,
             render_context: None,
         }
+    }
+
+    fn select_physical_device(
+        instance: &Arc<Instance>,
+        device_extensions: &DeviceExtensions,
+        event_loop: &EventLoop<()>,
+    ) -> (Arc<PhysicalDevice>, u32) {
+        instance
+            .enumerate_physical_devices()
+            .unwrap()
+            .filter(|p| p.supported_extensions().contains(device_extensions))
+            .filter_map(|device| {
+                device
+                    .queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|(i, queue_family)| {
+                        //i'm taking the first queue that satisfies the condition
+                        queue_family.queue_flags.contains(QueueFlags::GRAPHICS)
+                            && device.presentation_support(i as u32, &event_loop).unwrap()
+                    })
+                    .map(|i| (device, i as u32))
+            })
+            .min_by_key(|(device, _i)| match device.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::Cpu => 2,
+                PhysicalDeviceType::Other => 3,
+                _ => 4,
+            })
+            .unwrap()
     }
 }
 
@@ -46,13 +126,17 @@ impl ApplicationHandler for App {
                 .unwrap(),
         );
 
-        self.render_context = Some(RenderContext::new(event_loop, window));
+        self.render_context = Some(RenderContext::new(
+            self.device.clone(),
+            self.instance.clone(),
+            window,
+        ));
     }
 
     fn window_event(
         &mut self,
         event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: window::WindowId,
+        _window_id: window::WindowId,
         event: winit::event::WindowEvent,
     ) {
         let render_context = self.render_context.as_mut().unwrap(); //i don't remember how box, as_mut etc works
@@ -73,6 +157,81 @@ impl ApplicationHandler for App {
 
                 if render_context.recreate_swapchain {
                     render_context.recreate_swapchain();
+                }
+
+                let (image_index, suboptimal, acquire_future) = match swapchain::acquire_next_image(render_context.swapchain.clone(), None)
+                        .map_err(Validated::unwrap) //unclear syntax
+                    {
+                        Ok(r) => r,
+                        Err(VulkanError::OutOfDate) => {
+                            render_context.recreate_swapchain = true;
+                            return;
+                        }
+                        Err(e) => panic!("{e}"),
+                    };
+
+                if suboptimal {
+                    render_context.recreate_swapchain = true;
+                }
+
+                let clear_values = vec![Some([0.0, 0.0, 0.0, 1.0].into())];
+
+                let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
+                    self.command_buffer_allocator.clone(),
+                    self.queue.queue_family_index(),
+                    CommandBufferUsage::OneTimeSubmit,
+                )
+                .unwrap();
+
+                //builder rust design pattern
+                command_buffer_builder
+                    .begin_render_pass(
+                        RenderPassBeginInfo {
+                            clear_values,
+                            ..RenderPassBeginInfo::framebuffer(
+                                render_context.framebuffers[image_index as usize].clone(),
+                            )
+                        },
+                        SubpassBeginInfo {
+                            contents: SubpassContents::Inline,
+                            ..Default::default()
+                        },
+                    )
+                    .unwrap()
+                    .end_render_pass(Default::default())
+                    .unwrap();
+
+                let command_buffer = command_buffer_builder.build().unwrap();
+
+                let future = render_context
+                    .previous_future
+                    .take()
+                    .unwrap()
+                    .join(acquire_future)
+                    .then_execute(self.queue.clone(), command_buffer)
+                    .unwrap()
+                    .then_swapchain_present(
+                        self.queue.clone(),
+                        SwapchainPresentInfo::swapchain_image_index(
+                            render_context.swapchain.clone(),
+                            image_index,
+                        ),
+                    )
+                    .then_signal_fence_and_flush();
+
+                match future.map_err(Validated::unwrap) {
+                    //this part was not explained
+                    Ok(future) => {
+                        render_context.previous_future = Some(Box::new(future) as Box<_>);
+                    }
+                    Err(VulkanError::OutOfDate) => {
+                        render_context.recreate_swapchain = true;
+                        render_context.previous_future =
+                            Some(Box::new(sync::now(self.device.clone())) as Box<_>);
+                    }
+                    Err(e) => {
+                        panic!("failed to flush future: {e}");
+                    }
                 }
             }
             _ => (),
--- src/app/render_context/mod.rs
+++ src/app/render_context/mod.rs
@@ -1,20 +1,16 @@
 use std::sync::Arc;
 
 use vulkano::{
-    command_buffer::allocator::StandardCommandBufferAllocator,
-    device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo},
-    image::Image,
-    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    device::Device,
+    image::{view::ImageView, Image},
+    instance::Instance,
     pipeline::graphics::viewport::Viewport,
-    render_pass::{Framebuffer, RenderPass},
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass},
     single_pass_renderpass,
     swapchain::{Surface, Swapchain, SwapchainCreateInfo},
     sync::{self, GpuFuture},
-    VulkanLibrary,
 };
-use winit::{event_loop::EventLoop, window::Window};
-
-use super::App;
+use winit::window::Window;
 
 pub struct RenderContext {
     pub window: Arc<Window>,
@@ -26,57 +22,19 @@ pub struct RenderContext {
 }
 
 impl RenderContext {
-    pub fn new(event_loop: &EventLoop<()>, window: Arc<Window>) -> Self {
-        let library = VulkanLibrary::new().unwrap();
-        let extensions = Surface::required_extensions(event_loop).unwrap();
-
-        let instance = Instance::new(
-            library,
-            // structs are passed as argument to resemble how the Vulkan API works
-            InstanceCreateInfo {
-                enabled_extensions: extensions,
-                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY, //this is need to run on macOS trough MoltenVK
-                ..Default::default()
-            },
-        )
-        .unwrap();
-
-        let device_extensions = DeviceExtensions {
-            khr_swapchain: true,
-            ..DeviceExtensions::empty()
-        };
-
-        let (physical_device, queue_family_index) =
-            Self::select_physical_device(&instance, &device_extensions, &event_loop);
-
-        //logical/software device, queues associated to the device
-        let (device, mut queues) = Device::new(
-            physical_device,
-            DeviceCreateInfo {
-                enabled_extensions: device_extensions,
-                queue_create_infos: vec![QueueCreateInfo {
-                    queue_family_index,
-                    ..Default::default()
-                }],
-                ..Default::default()
-            },
-        )
-        .unwrap();
-
-        let queue = queues.next().unwrap(); //selecting a random queue
-
+    pub fn new(device: Arc<Device>, instance: Arc<Instance>, window: Arc<Window>) -> Self {
         let surface = Surface::from_window(instance.clone(), window.clone()).unwrap();
-        let (swapchain, images) = Self::get_swapchain(surface, window.clone());
-        let command_buffer_allocator =
-            StandardCommandBufferAllocator::new(device.clone(), Default::default());
-        let render_pass = Self::get_render_pass(swapchain.clone());
+        let (swapchain, images) = Self::get_swapchain(device.clone(), surface, window.clone());
+        let render_pass = Self::get_render_pass(device.clone(), swapchain.clone());
+
         //subpass and graphics pipeline
-        let viewport = Viewport {
+
+        let _viewport = Viewport {
             offset: [0.0, 0.0],
             extent: window.inner_size().into(),
             depth_range: 0.0..=1.0,
         };
-        let framebuffers = get_framebuffers(&images, render_pass.clone());
+        let framebuffers = Self::get_framebuffers(&images, render_pass.clone());
         let previous_future = Some(Box::new(sync::now(device.clone())) as Box<dyn GpuFuture>);
 
         Self {
@@ -89,58 +47,25 @@ impl RenderContext {
         }
     }
 
-    fn select_physical_device(
-        instance: &Arc<Instance>,
-        device_extensions: &DeviceExtensions,
-        event_loop: &EventLoop<()>,
-    ) -> (Arc<PhysicalDevice>, u32) {
-        instance
-            .enumerate_physical_devices()
-            .expect("failed to enumerate physical devices")
-            .filter(|p| p.supported_extensions().contains(device_extensions))
-            .filter_map(|device| {
-                device
-                    .queue_family_properties()
-                    .iter()
-                    .enumerate()
-                    .position(|(i, queue_family)| {
-                        //i'm taking the first queue that satisfies the condition
-                        queue_family.queue_flags.contains(QueueFlags::GRAPHICS)
-                            && device.presentation_support(i as u32, &event_loop).unwrap()
-                    })
-                    .map(|i| (device, i as u32))
-            })
-            .min_by_key(|(device, _i)| match device.properties().device_type {
-                PhysicalDeviceType::DiscreteGpu => 0,
-                PhysicalDeviceType::IntegratedGpu => 1,
-                PhysicalDeviceType::Cpu => 2,
-                PhysicalDeviceType::Other => 3,
-                _ => 4,
-            })
-            .unwrap()
-    }
-
     fn get_swapchain(
-        &self,
+        device: Arc<Device>,
         surface: Arc<Surface>,
         window: Arc<Window>,
     ) -> (Arc<Swapchain>, Vec<Arc<Image>>) {
-        let caps = self
-            .device
+        let caps = device
             .physical_device()
             .surface_capabilities(&surface, Default::default())
             .unwrap();
         let alpha = caps.supported_composite_alpha.into_iter().next().unwrap(); //another strange type wich i don't understand
 
-        let image_format = self
-            .device
+        let image_format = device
             .physical_device()
             .surface_formats(&surface, Default::default())
             .unwrap()[0]
             .0;
 
         Swapchain::new(
-            self.device.clone(),
+            device,
             surface,
             SwapchainCreateInfo {
                 min_image_count: caps.min_image_count,
@@ -154,9 +79,9 @@ impl RenderContext {
         .unwrap()
     }
 
-    fn get_render_pass(swapchain: Arc<Swapchain>) -> Arc<RenderPass> {
+    fn get_render_pass(device: Arc<Device>, swapchain: Arc<Swapchain>) -> Arc<RenderPass> {
         single_pass_renderpass!(
-            self.device.clone(),
+            device,
             attachments: {
                 color: {
                     format: swapchain.image_format(),
@@ -193,7 +118,8 @@ impl RenderContext {
             .collect::<Vec<_>>()
     }
 
-    pub fn recreate_swapchain(&self) {
+    pub fn recreate_swapchain(&mut self) {
+        //omitted match case to handle errors
         let (new_swapchain, new_images) = self
             .swapchain
             .recreate(SwapchainCreateInfo {
@@ -202,6 +128,7 @@ impl RenderContext {
             })
             .unwrap();
         self.swapchain = new_swapchain;
-        todo!()
+        self.framebuffers = Self::get_framebuffers(&new_images, self.render_pass.clone());
+        self.recreate_swapchain = false;
     }
 }
