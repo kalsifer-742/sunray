33a0b42ee9ffb233a627d71242389d430a8a70bc
Kalsifer-742 <kalsifer742@gmail.com>
2025-05-17 19:22:32 +0200

    initial stages of TLAS creation

--- src/vulkan_abstraction/buffer/mod.rs
+++ src/vulkan_abstraction/buffer/mod.rs
@@ -1,18 +1,23 @@
-mod mapped_memory;
 pub mod index_buffer;
+mod mapped_memory;
 pub mod vertex_buffer;
 
-use mapped_memory::*;
 pub use index_buffer::*;
+use mapped_memory::*;
 pub use vertex_buffer::*;
+//why use and not just mod?
+pub mod instance_buffer;
 
 use crate::vulkan_abstraction::queue::Queue;
 
-use std::ops::Deref;
-use ash::{vk, Device};
-use ash::vk::{BufferUsageFlags, CommandPool, DeviceMemory, DeviceSize, MemoryAllocateFlags, MemoryAllocateFlagsInfo, MemoryPropertyFlags, PhysicalDeviceMemoryProperties};
 use crate::error::*;
 use crate::vulkan_abstraction::cmd_buffer;
+use ash::vk::{
+    BufferUsageFlags, CommandPool, DeviceMemory, DeviceSize, MemoryAllocateFlags,
+    MemoryAllocateFlagsInfo, MemoryPropertyFlags, PhysicalDeviceMemoryProperties,
+};
+use ash::{Device, vk};
+use std::ops::Deref;
 
 pub struct Buffer {
     usable_byte_size: DeviceSize,
@@ -20,28 +25,54 @@ pub struct Buffer {
 
     buffer: vk::Buffer,
     memory: DeviceMemory,
-    mapped_memory: Option<RawMappedMemory>
+    mapped_memory: Option<RawMappedMemory>,
 }
 impl Buffer {
-    pub fn new_staging<V>(device: Device, size: usize, mem_props : &PhysicalDeviceMemoryProperties) -> SrResult<Self> {
+    pub fn new_staging<V>(
+        device: Device,
+        size: usize,
+        mem_props: &PhysicalDeviceMemoryProperties,
+    ) -> SrResult<Self> {
         let mem_flags =
             vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT;
         let mem_alloc_flags = MemoryAllocateFlags::empty();
         let usage_flags = vk::BufferUsageFlags::TRANSFER_SRC;
 
-        Self::new::<V>(device, size, mem_flags, mem_alloc_flags, usage_flags, mem_props)
+        Self::new::<V>(
+            device,
+            size,
+            mem_flags,
+            mem_alloc_flags,
+            usage_flags,
+            mem_props,
+        )
     }
-    
+
     #[allow(dead_code)]
-    pub fn new_uniform<T>(device: Device, mem_props : &PhysicalDeviceMemoryProperties) -> SrResult<Self> {
-        let mem_flags = vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT;
+    pub fn new_uniform<T>(
+        device: Device,
+        mem_props: &PhysicalDeviceMemoryProperties,
+    ) -> SrResult<Self> {
+        let mem_flags =
+            vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT;
         let mem_alloc_flags = MemoryAllocateFlags::empty();
         let usage_flags = vk::BufferUsageFlags::UNIFORM_BUFFER;
 
-        Self::new::<T>(device, 1, mem_flags, mem_alloc_flags, usage_flags, mem_props)
+        Self::new::<T>(
+            device,
+            1,
+            mem_flags,
+            mem_alloc_flags,
+            usage_flags,
+            mem_props,
+        )
     }
 
-    pub fn new_staging_from_data<T: Copy>(device: Device, data: &[T], mem_props : &PhysicalDeviceMemoryProperties) -> SrResult<Self> {
+    pub fn new_staging_from_data<T: Copy>(
+        device: Device,
+        data: &[T],
+        mem_props: &PhysicalDeviceMemoryProperties,
+    ) -> SrResult<Self> {
         //create staging buffer
         let mut staging_buffer = Self::new_staging::<T>(device, data.len(), mem_props)?;
 
@@ -53,8 +84,12 @@ impl Buffer {
         Ok(staging_buffer)
     }
     pub fn new<V>(
-        device: Device, len: usize, mem_flags: MemoryPropertyFlags, alloc_flags: MemoryAllocateFlags,
-        usage_flags: BufferUsageFlags, mem_props : &PhysicalDeviceMemoryProperties
+        device: Device,
+        len: usize,
+        mem_flags: MemoryPropertyFlags,
+        alloc_flags: MemoryAllocateFlags,
+        usage_flags: BufferUsageFlags,
+        mem_props: &PhysicalDeviceMemoryProperties,
     ) -> SrResult<Self> {
         let usable_byte_size = (len * size_of::<V>()) as vk::DeviceSize;
         let buffer = {
@@ -63,19 +98,19 @@ impl Buffer {
                 .usage(usage_flags)
                 .sharing_mode(vk::SharingMode::EXCLUSIVE);
 
-            unsafe { device.create_buffer(&buf_info, None)  }.map_err(SrError::from)?
+            unsafe { device.create_buffer(&buf_info, None) }.map_err(SrError::from)?
         };
 
         let mem_requirements = unsafe { device.get_buffer_memory_requirements(buffer) };
         let allocation_byte_size = mem_requirements.size;
         let mem_type_idx = {
             type BitsType = u32;
-            let bits : BitsType = mem_requirements.memory_type_bits;
+            let bits: BitsType = mem_requirements.memory_type_bits;
             assert_ne!(bits, 0);
 
             let mem_types = mem_props.memory_types;
             let mut idx = -1;
-            for i in 0..(8*size_of::<BitsType>()) {
+            for i in 0..(8 * size_of::<BitsType>()) {
                 let mem_type_is_supported = bits & (1 << i) != 0;
                 if mem_type_is_supported {
                     if mem_types[i].property_flags & mem_flags == mem_flags {
@@ -87,15 +122,14 @@ impl Buffer {
             if idx < 0 {
                 return Err(SrError::new(
                     None,
-                    String::from("Vertex Buffer Memory Type not supported!")
+                    String::from("Vertex Buffer Memory Type not supported!"),
                 ));
             }
 
             idx as u32
         };
 
-        let mut mem_alloc_flags_info = MemoryAllocateFlagsInfo::default()
-            .flags(alloc_flags);
+        let mut mem_alloc_flags_info = MemoryAllocateFlagsInfo::default().flags(alloc_flags);
 
         let mem_alloc_info = {
             let mem_alloc_info = vk::MemoryAllocateInfo::default()
@@ -108,26 +142,38 @@ impl Buffer {
                 mem_alloc_info.push_next(&mut mem_alloc_flags_info)
             }
         };
-        let memory = unsafe { device.allocate_memory(&mem_alloc_info, None)  }.map_err(SrError::from)?;
+        let memory =
+            unsafe { device.allocate_memory(&mem_alloc_info, None) }.map_err(SrError::from)?;
 
-        unsafe { device.bind_buffer_memory(buffer, memory, 0)  }.map_err(SrError::from)?;
+        unsafe { device.bind_buffer_memory(buffer, memory, 0) }.map_err(SrError::from)?;
 
-        Ok(Self { usable_byte_size, device, buffer, memory, mapped_memory: None })
+        Ok(Self {
+            usable_byte_size,
+            device,
+            buffer,
+            memory,
+            mapped_memory: None,
+        })
     }
 
-    pub fn byte_size(&self) -> DeviceSize { self.usable_byte_size }
+    pub fn byte_size(&self) -> DeviceSize {
+        self.usable_byte_size
+    }
     pub fn map<V>(&mut self) -> SrResult<&mut [V]> {
         let flags = vk::MemoryMapFlags::empty();
-        let p = unsafe { self.device.map_memory(self.memory, 0, self.usable_byte_size, flags)  }.map_err(SrError::from)?;
+        let p = unsafe {
+            self.device
+                .map_memory(self.memory, 0, self.usable_byte_size, flags)
+        }
+        .map_err(SrError::from)?;
         let raw_mem = unsafe { RawMappedMemory::new(p, self.usable_byte_size as usize) };
         self.mapped_memory = Some(raw_mem);
         let ret = self.mapped_memory.as_mut().unwrap().borrow();
 
-
         Ok(ret)
     }
 
-    // correctness of unmap is checked by the borrow checker: it only works if the previous 
+    // correctness of unmap is checked by the borrow checker: it only works if the previous
     // mut borrow of self was already dropped. drop() calls unmap() if necessary
     pub fn unmap(&mut self) {
         self.mapped_memory = None;
@@ -136,29 +182,34 @@ impl Buffer {
     }
 
     // mainly useful to copy from a staging buffer to a device buffer
-    pub fn clone_buffer(device: &Device, queue: &Queue, cmd_pool: &CommandPool, src: &Buffer, dst: &Buffer) -> SrResult<()>{
+    pub fn clone_buffer(
+        device: &Device,
+        queue: &Queue,
+        cmd_pool: &CommandPool,
+        src: &Buffer,
+        dst: &Buffer,
+    ) -> SrResult<()> {
         let bufcpy_cmd_buf = cmd_buffer::new(cmd_pool, device)?;
 
         let begin_info = vk::CommandBufferBeginInfo::default()
             .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
 
-        unsafe { device.begin_command_buffer(bufcpy_cmd_buf, &begin_info)  }.map_err(SrError::from)?;
+        unsafe { device.begin_command_buffer(bufcpy_cmd_buf, &begin_info) }
+            .map_err(SrError::from)?;
 
         debug_assert!(src.byte_size() <= dst.byte_size());
 
-        let regions = [
-            vk::BufferCopy::default()
-                .size(src.byte_size())
-                .src_offset(0)
-                .dst_offset(0)
-        ];
+        let regions = [vk::BufferCopy::default()
+            .size(src.byte_size())
+            .src_offset(0)
+            .dst_offset(0)];
 
         unsafe { device.cmd_copy_buffer(bufcpy_cmd_buf, **src, **dst, &regions) };
 
-        unsafe { device.end_command_buffer(bufcpy_cmd_buf)  }.map_err(SrError::from)?;
+        unsafe { device.end_command_buffer(bufcpy_cmd_buf) }.map_err(SrError::from)?;
 
         queue.submit_sync(bufcpy_cmd_buf)?;
-        unsafe { device.device_wait_idle()  }.map_err(SrError::from)?;
+        unsafe { device.device_wait_idle() }.map_err(SrError::from)?;
 
         unsafe { device.free_command_buffers(*cmd_pool, &[bufcpy_cmd_buf]) };
         // vk_core.get_queue().wait_idle()?;
@@ -167,10 +218,10 @@ impl Buffer {
     }
 
     pub fn get_device_address(&self) -> vk::DeviceAddress {
-        let buffer_device_address_info = vk::BufferDeviceAddressInfo::default()
-            .buffer(self.buffer);
+        let buffer_device_address_info = vk::BufferDeviceAddressInfo::default().buffer(self.buffer);
         unsafe {
-            self.device.get_buffer_device_address(&buffer_device_address_info)
+            self.device
+                .get_buffer_device_address(&buffer_device_address_info)
         }
     }
 }
@@ -181,12 +232,18 @@ impl Drop for Buffer {
             self.unmap();
         }
 
-        unsafe { self.device.destroy_buffer(self.buffer, None); }
-        unsafe { self.device.free_memory(self.memory, None); }
+        unsafe {
+            self.device.destroy_buffer(self.buffer, None);
+        }
+        unsafe {
+            self.device.free_memory(self.memory, None);
+        }
     }
 }
 
 impl Deref for Buffer {
     type Target = vk::Buffer;
-    fn deref(&self) -> &Self::Target { &self.buffer }
+    fn deref(&self) -> &Self::Target {
+        &self.buffer
+    }
 }
--- src/vulkan_abstraction/mod.rs
+++ src/vulkan_abstraction/mod.rs
@@ -1,9 +1,10 @@
 pub mod blas;
 pub mod buffer;
-pub mod queue;
 pub mod cmd_pool;
+pub mod queue;
+mod tlas;
 
 pub use blas::*;
 pub use buffer::*;
-pub use queue::*;
 pub use cmd_pool::*;
+pub use queue::*;
